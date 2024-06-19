65a37a86042b2ad6d7b42f1a88874aef1d59e6ca
afqhrosli <88609003+afqhrosli@users.noreply.github.com>
2024-06-20 00:36:18 +0800

    Add files via upload

---
+++ bsm_db (3).sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 19, 2024 at 06:28 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bsm_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+-- Error reading structure for table bsm_db.book: #1932 - Table &#039;bsm_db.book&#039; doesn&#039;t exist in engine
+-- Error reading data for table bsm_db.book: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bsm_db`.`book`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+-- Error reading structure for table bsm_db.member: #1932 - Table &#039;bsm_db.member&#039; doesn&#039;t exist in engine
+-- Error reading data for table bsm_db.member: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bsm_db`.`member`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+-- Error reading structure for table bsm_db.order: #1932 - Table &#039;bsm_db.order&#039; doesn&#039;t exist in engine
+-- Error reading data for table bsm_db.order: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bsm_db`.`order`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+-- Error reading structure for table bsm_db.payment: #1932 - Table &#039;bsm_db.payment&#039; doesn&#039;t exist in engine
+-- Error reading data for table bsm_db.payment: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bsm_db`.`payment`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table bsm_db.user: #1932 - Table &#039;bsm_db.user&#039; doesn&#039;t exist in engine
+-- Error reading data for table bsm_db.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `bsm_db`.`user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
